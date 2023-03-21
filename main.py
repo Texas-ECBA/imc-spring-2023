cadaee0203fe1e6b20e03da76b99d37b72af9077
Saatvik Aggarwal <hashtagsaatvikagg@gmail.com>
2023-03-20 22:28:36 -0500

    minimum working version PEARLS ONLY

--- main.py
+++ main.py
@@ -73,6 +73,7 @@ class ProsperityEncoder(JSONEncoder):
 
 
 class Trader:
+    maxPositionQuantity: int = 20
 
     def run(self, state: TradingState) -> Dict[str, List[Order]]:
         """
@@ -84,7 +85,17 @@ class Trader:
 
         # Iterate over all the keys (the available products) contained in the order depths
         for product in state.order_depths.keys():
+            currentProductAmount = 0
 
+            try:
+                currentProductAmount = state.position[product]
+            except:
+                pass
+
+            print("AT TIME ", state.timestamp, "PRODUCT ", product, " HAS POSITION: ", currentProductAmount)
+            # if product == "BANANAS":
+            #     pass
+            
             # Check if the current product is the 'PEARLS' product, only then run the order logic
             if product == 'PEARLS':
 
@@ -101,32 +112,77 @@ class Trader:
                 # If statement checks if there are any SELL orders in the PEARLS market
                 if len(order_depth.sell_orders) > 0:
 
-                    # Sort all the available sell orders by their price,
-                    # and select only the sell order with the lowest price
-                    best_ask = min(order_depth.sell_orders.keys())
-                    best_ask_volume = order_depth.sell_orders[best_ask]
+                    # # Sort all the available sell orders by their price,
+                    # # and select only the sell order with the lowest price
+                    # best_ask = min(order_depth.sell_orders.keys())
+                    # best_ask_volume = order_depth.sell_orders[best_ask]
 
-                    # Check if the lowest ask (sell order) is lower than the above defined fair value
-                    if best_ask < acceptable_price:
+                    # # Check if the lowest ask (sell order) is lower than the above defined fair value
+                    # if best_ask <= acceptable_price:
+                    #     print("AT TIME ", state.timestamp, "PRODUCT ", product, " HAS SELL ORDERS: ", state.order_depths[product].sell_orders)
 
-                        # In case the lowest ask is lower than our fair value,
-                        # This presents an opportunity for us to buy cheaply
-                        # The code below therefore sends a BUY order at the price level of the ask,
-                        # with the same quantity
-                        # We expect this order to trade with the sell order
-                        print("BUY", str(-best_ask_volume) + "x", best_ask)
-                        orders.append(Order(product, best_ask, -best_ask_volume))
+                    #     # In case the lowest ask is lower than our fair value,
+                    #     # This presents an opportunity for us to buy cheaply
+                    #     # The code below therefore sends a BUY order at the price level of the ask,
+                    #     # with the same quantity
+                    #     # We expect this order to trade with the sell order
+                    #     print("BUY", product, str(-best_ask_volume) + "x", best_ask)
+                    #     orders.append(Order(product, best_ask, -best_ask_volume))
+
+                    # Go through all the orders and match all favorable orders
+                    print("AT TIME ", state.timestamp, "PRODUCT ", product, " HAS SELL ORDERS: ", state.order_depths[product].sell_orders)
+
+                    # Loop through all the sell orders, but first sorted by price descending
+                    # sell_orders is a dictionary with price as key and quantity as value
+                    possiblePrices = sorted(order_depth.sell_orders.keys(), reverse=True)
+
+                    for order in possiblePrices:
+                        if order < acceptable_price:
+
+                            # In case the lowest ask is lower than our fair value,
+                            # This presents an opportunity for us to buy cheaply
+                            # The code below therefore sends a BUY order at the price level of the ask,
+                            # with the same quantity
+                            # We expect this order to trade with the sell order
+                            possibleQuantity: int = -1 * order_depth.sell_orders[order]
+                            if possibleQuantity + currentProductAmount > self.maxPositionQuantity:
+                                possibleQuantity = self.maxPositionQuantity - currentProductAmount
+
+                            print("TRYING TO BUY", product, str(possibleQuantity) + "x", order)
+                            orders.append(Order(product, order, possibleQuantity))
+
 
                 # The below code block is similar to the one above,
                 # the difference is that it finds the highest bid (buy order)
                 # If the price of the order is higher than the fair value
                 # This is an opportunity to sell at a premium
                 if len(order_depth.buy_orders) != 0:
-                    best_bid = max(order_depth.buy_orders.keys())
-                    best_bid_volume = order_depth.buy_orders[best_bid]
-                    if best_bid > acceptable_price:
-                        print("SELL", str(best_bid_volume) + "x", best_bid)
-                        orders.append(Order(product, best_bid, -best_bid_volume))
+                    # best_bid = max(order_depth.buy_orders.keys())
+                    # best_bid_volume = order_depth.buy_orders[best_bid]
+                    # if best_bid >= acceptable_price:
+                    #     print("AT TIME ", state.timestamp, "PRODUCT ", product, " HAS BUY ORDERS: ", state.order_depths[product].buy_orders)
+
+                    #     print("SELL", product, str(best_bid_volume) + "x", best_bid)
+                    #     orders.append(Order(product, best_bid, -best_bid_volume))
+
+                    print("AT TIME ", state.timestamp, "PRODUCT ", product, " HAS BUY ORDERS: ", state.order_depths[product].buy_orders)
+
+                    possiblePrices: list[int] = sorted(order_depth.buy_orders.keys(), reverse=False)
+
+                    for order in possiblePrices:
+                        if order > acceptable_price:
+
+                            # In case the lowest ask is lower than our fair value,
+                            # This presents an opportunity for us to buy cheaply
+                            # The code below therefore sends a BUY order at the price level of the ask,
+                            # with the same quantity
+                            # We expect this order to trade with the sell order
+                            possibleQuantity: int = -1 * order_depth.buy_orders[order] # some negative number
+                            if possibleQuantity + currentProductAmount < -1 * self.maxPositionQuantity:
+                                possibleQuantity = -1 * self.maxPositionQuantity - currentProductAmount
+
+                            print("TRYING TO SELL", product, str(possibleQuantity) + "x", order)
+                            orders.append(Order(product, order, possibleQuantity))
 
                 # Add all the above orders to the result dict
                 result[product] = orders
